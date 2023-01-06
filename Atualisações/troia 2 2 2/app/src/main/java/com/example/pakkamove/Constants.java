bdfe6173af7bffdb885b2cbd6992989fae12e0c5
EULANANI_ECOMBE <eularinanisidneyeconbe@gmail.com>
2023-01-06 23:00:54 +0000

    Banco a saltar para o marker correto no mapa Alterar estado de um marker e ver alterações Put para o servidor //TODO: ver

--- Atualisações/troia 2 2 2/app/src/main/java/com/example/pakkamove/Constants.java
+++ Atualisações/troia 2 2 2/app/src/main/java/com/example/pakkamove/Constants.java
@@ -6,7 +6,8 @@ public class Constants {
     public static final String IP_LOCAL = "192.168.1.247";
     public static final String API_TIPOMAC = ":8080/api/tipomac/";
     public static final String API_M_ESTADO = ":8080/api/maquina/Mestados";
-    public static final String API_PUT_ESTADO = ":8080/api/path";
+    public static final String API_PUT_ESTADO = ":8080/api/usm/atualizarestado";
+    public static final String USER_API = ":8080/api/usuarios/login";
 
 
 }
